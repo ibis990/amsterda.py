e313d7cb27e31918a6867dab00a141516df13ede
ibis990 <132020590+ibis990@users.noreply.github.com>
2023-05-06 18:02:50 -0300

    Add files via upload

---
+++ Amsterdã premium.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import requests
+
+ip = input("Insira o IP: ")
+
+url = "http://ip-api.com/json/" + ip
+
+response = requests.get(url)
+data = response.json()
+
+print("País: " + data["country"])
+print("Cidade: " + data["city"])
+print("Região: " + data["regionName"])
+print('Bairro :', data['isp'])
+print('Rua    :', data['query'])
+print("Latitude: " + str(data["lat"]))
+print("Longitude: " + str(data["lon"]))
+print("Organização: " + data["org"])
+print("Sistema Operacional: " + data["os"])
+print("Comandos Android: " + data["androidCommands"])
